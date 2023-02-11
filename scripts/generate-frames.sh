a9dffce899dbdf04ab24524c14fd3052c8b2a5c5
ras0q <ras0q@users.noreply.github.com>
2023-02-11 14:01:50 +0900

    :sparkles: load frames(auto generated) at first

---
+++ scripts/generate-frames.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DIST_DIR=./src/generated/frames
+
+if [ -d "$DIST_DIR" ]; then
+  echo "Removing $DIST_DIR"
+  rm -rf $DIST_DIR
+  mkdir -p $DIST_DIR
+fi
+
+echo "Generating frames from video"
+ffmpeg -i ./src/assets/sample-from-adobe.mp4 -r 30 -f image2 $DIST_DIR/frame%05d.png
+
+echo "Generating index.ts"
+FILES=$(ls $DIST_DIR | sed 's/.png//g')
+echo "import * as THREE from 'three'" > $DIST_DIR/index.ts
+for f in $FILES; do
+  echo "import $f from './$f.png'" >> $DIST_DIR/index.ts
+done
+echo "" >> $DIST_DIR/index.ts
+echo "const loader = new THREE.TextureLoader()" >> $DIST_DIR/index.ts
+echo "const frames = [" >> $DIST_DIR/index.ts
+for f in $FILES; do
+  echo "  $f," >> $DIST_DIR/index.ts
+done
+echo "].map(f => loader.load(f))" >> $DIST_DIR/index.ts
+echo "" >> $DIST_DIR/index.ts
+echo "export default frames" >> $DIST_DIR/index.ts
+
+echo "Done"
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { OrbitControls } from '@react-three/drei'
-import { Canvas, RootState, useFrame, useLoader } from '@react-three/fiber'
+import { Canvas, RootState, useFrame } from '@react-three/fiber'
 import { Suspense, useMemo, useState } from 'react'
 import * as THREE from 'three'
 import './App.css'
-import { frames } from './frames'
+import frames from './generated/frames'
 
 function App() {
   const createdHandler = (state: RootState) => {
@@ -38,9 +38,10 @@ function App() {
 export default App
 
 const Clipper = () => {
-  const _textures = useLoader(THREE.TextureLoader, frames)
   const [normX, setNormX] = useState(0)
-  const [textures, setTextures] = useState(_textures)
+  const allTextures = useMemo(() => frames, [])
+  const frameLimit = useMemo(() => 100, [])
+  const [textures, setTextures] = useState(allTextures.slice(0, frameLimit))
 
   const clipPlanes = useMemo(() => {
     return [new THREE.Plane(new THREE.Vector3(-1, 0, normX), 0)]
@@ -49,15 +50,25 @@ const Clipper = () => {
   useFrame(({ clock }) => {
     setNormX(Math.cos(clock.getElapsedTime()))
 
-    const lastFrame = textures[textures.length - 1]
-    const newFrames = [lastFrame, ...textures.slice(0, textures.length - 1)]
+    const frameIndex = Math.floor(clock.getElapsedTime() * 30)
+    const restIndex = frameIndex + frameLimit - allTextures.length
+    const newFrames =
+      restIndex > 0
+        ? allTextures
+            .slice(frameIndex, allTextures.length)
+            .concat(allTextures.slice(0, restIndex))
+        : allTextures.slice(frameIndex, frameIndex + frameLimit)
     setTextures(newFrames)
   })
 
   return (
     <group>
       {textures.map((texture, i) => (
-        <mesh key={i} castShadow position={[i * (10 / textures.length), 0, 0]}>
+        <mesh
+          key={i.toString()}
+          castShadow
+          position={[i * (10 / textures.length), 0, 0]}
+        >
           <boxGeometry args={[10, 10, 10]} />
           <meshStandardMaterial
             map={texture}
