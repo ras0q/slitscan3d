1c3ea2d57436386b152f114bd3e4f6d70b832256
ras0q <ras0q@users.noreply.github.com>
2024-03-03 23:50:15 +0900

    :zap: use needsUpdate instead of setState

--- src/components/SlitScan/SlitScanGroup.tsx
+++ src/components/SlitScan/SlitScanGroup.tsx
@@ -1,6 +1,14 @@
 import { useFrame } from '@react-three/fiber'
-import { useEffect, useMemo, useRef, useState } from 'react'
-import { DataTexture, DoubleSide, MeshStandardMaterial, Plane, RGBAFormat, Texture, Vector3 } from 'three'
+import { useEffect, useMemo, useRef } from 'react'
+import {
+  DataTexture,
+  DoubleSide,
+  Plane,
+  RGBAFormat,
+  Texture,
+  Vector3,
+  MeshStandardMaterial,
+} from 'three'
 
 type SlitScanGroupProps = {
   video: HTMLVideoElement
@@ -14,7 +22,7 @@ type SlitScanGroupProps = {
 export const SlitScanGroup = ({ video, width, height, depth, frameLimit, clipPlanes }: SlitScanGroupProps) => {
   if (!video) return null
 
-  const [textures, setTextures] = useState<Texture[]>([])
+  const texturesRef = useRef<Texture[]>([])
   const reqIdRef = useRef(0)
 
   useEffect(() => {
@@ -24,8 +32,7 @@ export const SlitScanGroup = ({ video, width, height, depth, frameLimit, clipPla
       })
       const texture = new Texture(imageBitmap)
       texture.needsUpdate = true
-
-      setTextures((prev) => (video.paused ? prev : [...prev, texture]))
+      texturesRef.current.push(texture)
 
       if (video.paused) {
         console.log('capture completed!!!')
@@ -38,15 +45,15 @@ export const SlitScanGroup = ({ video, width, height, depth, frameLimit, clipPla
     reqIdRef.current = video.requestVideoFrameCallback(pushFrame)
 
     return () => {
-      textures.forEach((texture) => texture.dispose())
+      texturesRef.current.forEach((texture) => texture.dispose())
       video.cancelVideoFrameCallback(reqIdRef.current)
     }
-  }, [video, textures])
+  }, [video])
 
   const fps = (video.playbackRate * video.getVideoPlaybackQuality().totalVideoFrames) / video.currentTime
   const offsetRef = useRef(0)
   const materialRefs = Array.from({ length: frameLimit }, () =>
-    useRef<MeshStandardMaterial>(
+    useRef(
       new MeshStandardMaterial({
         map: new DataTexture(null, width, height, RGBAFormat) as Texture,
       }),
@@ -54,16 +61,17 @@ export const SlitScanGroup = ({ video, width, height, depth, frameLimit, clipPla
   )
 
   useFrame((_, delta) => {
-    if (textures.length === 0) return
+    if (texturesRef.current.length === 0) return
 
-    const canRotateFrames = video.paused || textures.length >= frameLimit
+    const canRotateFrames = video.paused || texturesRef.current.length >= frameLimit
     if (canRotateFrames) {
-      offsetRef.current = (offsetRef.current + fps * delta) % textures.length
+      offsetRef.current = (offsetRef.current + fps * delta) % texturesRef.current.length
     }
 
     materialRefs.forEach((ref, i) => {
-      if (!canRotateFrames && i > textures.length - 1) return
-      ref.current.map = textures[(i + Math.floor(offsetRef.current)) % textures.length]
+      if (!canRotateFrames && i > texturesRef.current.length - 1) return
+      ref.current.needsUpdate = true
+      ref.current.map = texturesRef.current[(i + Math.floor(offsetRef.current)) % texturesRef.current.length]
     })
   })
 
@@ -71,10 +79,9 @@ export const SlitScanGroup = ({ video, width, height, depth, frameLimit, clipPla
   const additionalClipPlane = useMemo(() => new Plane(new Vector3(0, 0, -1), Math.ceil(depth / 2)), [depth])
 
   return (
-    <group receiveShadow>
+    <group>
       {materialRefs.map((ref, i) => (
         <mesh
-          castShadow
           key={ref.current.id}
           position={[0, 0, depth - i * (depth / materialRefs.length)]}
           rotation={[Math.PI, 0, 0]} // NOTE: imageBitmap is roteated 180 degrees
